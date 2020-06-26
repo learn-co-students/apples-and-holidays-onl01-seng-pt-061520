d528a4fe9b58144e7354337f854891fbd53442e5
Cameron Hudson <camnn3@yahoo.com>
2020-06-26 13:58:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts #{season.capitalize):
+    puts "#{season.capitalize):"
       holiday.each do |holiday, item|
+        puts "#{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
     
 end
 
