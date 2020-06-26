df4c72b52866e9b8fbd16ab2b38ddc98914437ca
Cameron Hudson <camnn3@yahoo.com>
2020-06-26 14:01:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
- 
+   holiday_array = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |attribute, data|
+      data.each do |item|
+        if item == "BBQ"
+          holiday_array << attribute
+        end
+      end
+    end
 end
 
 
