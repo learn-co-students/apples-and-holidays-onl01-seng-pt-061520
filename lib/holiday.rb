acd0114dadd1005c59eaf2ec31b241236e06a96b
Cameron Hudson <camnn3@yahoo.com>
2020-06-26 13:48:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash[:spring][:memorial_day] << "Table Cloth"
+  holiday_hash[:spring][:memorial_day] = supply_array
     return holiday_hash
 end
 
