e3d3c173a730f848ba3d88bc3f1b6f3955c0e874
Cameron Hudson <camnn3@yahoo.com>
2020-06-26 13:57:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts #{season.capitalize):
-      holiday.each do |
+      holiday.each do |holiday, item|
     
 end
 
