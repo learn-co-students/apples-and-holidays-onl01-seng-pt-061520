3c0eb7f1bf4f1f6890a5b24fcb876f597d71f49a
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 22:45:25 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,4 +1,3 @@
-
 require 'pry'
 def second_supply_for_fourth_of_july(holiday_hash)  # given that holiday_hash looks like this:  # {  #   :winter => {  #     :christmas => ["Lights", "Wreath"],  #     :new_years => ["Party Hats"]  #   },  #   :summer => {  #     :fourth_of_july => ["Fireworks", "BBQ"]  #   },  #   :fall => {  #     :thanksgiving => ["Turkey"]  #   },  #   :spring => {  #     :memorial_day => ["BBQ"]  #   }  # }  # return the second element in the 4th of July array  holiday_hash[:summer][:fourth_of_july][1]end
 def add_supply_to_winter_holidays(holiday_hash, supply)  # holiday_hash is identical to the one above  # add the second argument, which is a supply, to BOTH the  # Christmas AND the New Year's arrays  holiday_hash[:winter][:christmas] << supply  holiday_hash[:winter][:new_years]<< supply
@@ -10,12 +9,21 @@ end
 def all_winter_holiday_supplies(holiday_hash)  # return an array of all of the supplies that are used in the winter season  holiday_hash[:winter].values.flatten
 end
 def all_supplies_in_holidays(holiday_hash)  # iterate through holiday_hash and print items such that your readout resembles:  # Winter:  #   Christmas: Lights, Wreath  #   New Years: Party Hats  # Summer:  #   Fourth Of July: Fireworks, BBQ  # etc.  holiday_hash.each do |season, data|    puts "#{season.to_s.capitalize!}:"    data.each do |holiday, supply|      array = holiday.to_s.split("_")      final_holiday = []        array.each do |x|          final_holiday << x.capitalize!        end      holiday = final_holiday.join(" ")
-      supply = supply.join(", ")      puts "  #{holiday}: #{supply}"
-    end  end
+      supply = supply.join(", ")     
+      puts "  #{holiday}: #{supply}"
+    end 
+    
+  end
 end
 def all_holidays_with_bbq(holiday_hash)  # return an array of holiday names (as symbols) where supply lists  # include the string "BBQ"
-  answer = []holiday_hash.each do |season, data|  data.each do |holiday, value|    value.each do |supply|     if supply == "BBQ"        answer.push(holiday)      end    end  endend	answerend
-
+ 
+  answer = []holiday_hash.each do |season, data| 
+    data.each do |holiday, value|    
+      value.each do |supply|     
+        if supply == "BBQ"        
+          answer.push(holiday)      
+          end    
+end  endend	answerend
 
 
 
