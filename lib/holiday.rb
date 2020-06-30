a5330503c272e95c271ed5b165ce50e3aa280422
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-30 18:31:09 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
 
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
-      holiday if supplies. include?("BBQ")
+      holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
 end
