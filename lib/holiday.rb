fd4c0f08664f2c381eae892e4d376618f5695bc0
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-30 18:33:55 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holidays|
-    #puts "#{season.capitalize}:"
+    puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
       puts "  #{holiday.to_s.split('_').map {|w| w.capitalize}.join(' ')}: #{supplies.join(", ")}"
     end
